2b97d6cef4829f64dc900ef98b2907477e12437e
Alec Hartman <aleclhartman08@gmail.com>
2020-06-16 15:40:20 -0500

    made more templates

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 import numpy as np
 
@@ -11,12 +11,37 @@ def index():
 
 @app.route('/roll-dice/<int:n>')
 def roll_dice(n):
-
     rolls = [str(np.random.choice(range(1, 7))) for i in range(n)]
-
     return render_template('roll_dice.html', rolls=rolls)
 
 @app.route('/say/<greeting>/to/<name>')
 def sayhelloto(greeting, name):
     message = f'{greeting}, {name}!'
     return render_template('greeting.html', message=message)
+
+@app.route('/order-pizza')
+def show_order_pizza_form():
+    return render_template('order-pizza-form.html')
+
+@app.route('/order-pizza', methods=['POST'])
+def handle_pizza_order_submission():
+
+    topping1 = request.form['topping1']
+    topping2 = request.form['topping2']
+    pizza = f'{topping1} and {topping2}'
+
+    return f'One pizza with {pizza} on the way!'
+
+@app.route('/count-to')
+def count_to():
+    return render_template('counting-form.html')
+
+@app.route('/count-to', methods=['POST'])
+def handle_counting_form_submission():
+    n = int(request.form['n'])
+    numbers = range(1, n + 1)
+
+    return render_template(
+        'counting-results.html',
+        numbers=numbers
+    )
